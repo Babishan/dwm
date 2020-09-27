e50b1b2acf1cb499d7121af26916a1d1ebf2d00a
Babishan <thashan@archtest.localdomain>
2020-09-27 20:40:31 +0200

    Changed terminal

--- bash/scripts/dropdown.sh
+++ bash/scripts/dropdown.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PID=$(ps -ef |grep alacritty |grep dropdown |awk '{print $2}')
-[ -z "$PID" ] && (alacritty --title dropdown --position 0 0 --dimensions 152 20 &) || kill -n 9 $PID
+PID=$(ps -ef |grep st |grep dropdown |awk '{print $2}')
+[ -z "$PID" ] && (st -T dropdown -g 198X20+0+0 &) || kill -n 9 $PID
